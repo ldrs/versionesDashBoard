7994924ecaf79f0a730ca14d282e36f404c2d6e3
pgermosen <depende@gmail.com>
2015-06-19 01:26:48 -0400

    Agregando primera fase servicios

---
+++ src/main/java/rd/huma/dashboard/servicios/WSAmbientes.java
@@ -0,0 +1,47 @@
+package rd.huma.dashboard.servicios;
+
+import static javax.json.Json.createArrayBuilder;
+import static javax.json.Json.createObjectBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.json.JsonArrayBuilder;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+
+import rd.huma.dashboard.model.EntAmbiente;
+
+@Path("/ambientes")
+public class WSAmbientes {
+
+	@GET
+	public String ambientes(@QueryParam("aplicacion") String aplicacion){
+		JsonArrayBuilder builder = createArrayBuilder();
+		getAmbientes().stream().forEach(s -> builder.add(createObjectBuilder()
+																				.add("nombre", s.getNombre())
+																				.add("css", "")
+																				.add("id",s.getId())
+																				.add("orden",s.getOrden())
+														 )
+										);
+		
+		return builder.build().toString();
+	}
+	
+	private List<EntAmbiente> getAmbientes(){
+		List<EntAmbiente> lst = new ArrayList<>();
+		lst.add(nuevoAmbiente("desarollo"));
+		lst.add(nuevoAmbiente("prueba"));
+		lst.add(nuevoAmbiente("mesa de ayuda"));
+		lst.add(nuevoAmbiente("produccion"));
+		return lst;
+	}
+	
+	private EntAmbiente nuevoAmbiente(String nombre){
+		EntAmbiente servidor = new EntAmbiente();
+		servidor.setNombre(nombre);
+		return servidor;
+	}
+}
---
+++ src/main/java/rd/huma/dashboard/servicios/WSAplicaciones.java
@@ -0,0 +1,44 @@
+package rd.huma.dashboard.servicios;
+
+import static javax.json.Json.createArrayBuilder;
+import static javax.json.Json.createObjectBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.json.JsonArrayBuilder;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import rd.huma.dashboard.model.EntAplicacion;
+
+@Path("/aplicaciones")
+public class WSAplicaciones {
+
+	@GET
+	public String aplicaciones(){
+		JsonArrayBuilder builder = createArrayBuilder();
+		getAmbientes().stream().forEach(s -> builder.add(createObjectBuilder()
+																				.add("nombre", s.getNombre())
+																				.add("css", "")
+																				.add("id",s.getId())
+																				.add("orden",s.getOrden())
+														 )
+										);
+		
+		return builder.build().toString();
+	}
+	
+	private List<EntAplicacion> getAmbientes(){
+		List<EntAplicacion> lst = new ArrayList<>();
+		lst.add(nuevoAmbiente("sigef"));
+		lst.add(nuevoAmbiente("esigef"));
+		return lst;
+	}
+	
+	private EntAplicacion nuevoAmbiente(String nombre){
+		EntAplicacion servidor = new EntAplicacion();
+		servidor.setNombre(nombre);
+		return servidor;
+	}
+}
