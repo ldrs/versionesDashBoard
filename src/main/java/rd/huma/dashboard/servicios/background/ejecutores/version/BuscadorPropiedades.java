1610d75d3cdc6bc693919193f8da36609d7a654e
Priamo Germosen <depende@gmail.com>
2015-06-29 17:58:58 -0400

    Procesamiento para propiedades de branch

---
+++ src/main/java/rd/huma/dashboard/servicios/background/ejecutores/version/BuscadorPropiedades.java
@@ -0,0 +1,24 @@
+package rd.huma.dashboard.servicios.background.ejecutores.version;
+
+import rd.huma.dashboard.model.EntAplicacion;
+import rd.huma.dashboard.model.EntConfiguracionGeneral;
+import rd.huma.dashboard.model.EntVersion;
+
+public class BuscadorPropiedades {
+
+	private EntConfiguracionGeneral entConfiguracionGeneral;
+	private EntAplicacion aplicacion;
+	private EntVersion version;
+
+
+	public BuscadorPropiedades(EntConfiguracionGeneral entConfiguracionGeneral,	EntAplicacion aplicacion, EntVersion version) {
+		this.entConfiguracionGeneral = entConfiguracionGeneral;
+		this.aplicacion = aplicacion;
+		this.version = version;
+	}
+
+	public void procesar(){
+
+	}
+
+}
