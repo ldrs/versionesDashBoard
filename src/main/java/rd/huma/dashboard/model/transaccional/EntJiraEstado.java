4ebb0f34070c4768dc5bf91e98cd8390eadef098
pgermosen <depende@gmail.com>
2015-07-07 17:16:26 -0400

    EstadoJira

---
+++ src/main/java/rd/huma/dashboard/model/transaccional/EntJiraEstado.java
@@ -0,0 +1,50 @@
+package rd.huma.dashboard.model.transaccional;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class EntJiraEstado extends AEntModelo {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2222206705107397270L;
+
+	@JoinColumn
+	@ManyToOne
+	private EntJira jira;
+	
+	private String estado;
+	
+
+	private LocalDateTime momentoCreacion = LocalDateTime.now();
+
+
+	public EntJira getJira() {
+		return jira;
+	}
+
+
+	public void setJira(EntJira jira) {
+		this.jira = jira;
+	}
+
+
+	public String getEstado() {
+		return estado;
+	}
+
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public LocalDateTime getMomentoCreacion() {
+		return momentoCreacion;
+	}
+	
+	
+}
